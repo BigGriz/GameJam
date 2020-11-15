efdab1b2f5477e65172cf3e32677a140517f6679
BigGriz <waydbarton@hotmail.co.nz>
2020-11-15 22:36:11 +1300

    Cash Updated

---
+++ Assets/CashRotator.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CashRotator : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.up * 200 * Time.deltaTime);
+        transform.localScale = Vector3.one * (Mathf.PingPong(Time.time, 0.5f) + 0.6f);
+    }
+}
